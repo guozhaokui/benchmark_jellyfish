a56361950f73e8b6478fc3d17ce47a4a3c40f5ec
guozhaokui <guozk@layabox.com>
2018-05-07 21:29:16 +0800

    继续简化

--- js/initWin.js
+++ js/initWin.js
@@ -3,10 +3,39 @@
 var gl;
 var canvas,docWidth,docHeight;
 
+function patchGL(gl){
+    //shader: WebGLShader | null, pname: number
+    gl.getShaderParameter=function(shader,pname){
+        switch(pname){
+            case gl.COMPILE_STATUS:return true;
+            default: return true;
+        }
+    }
+
+    //getProgramParameter(program: WebGLProgram | null, pname: number): any;
+    //  gl.LINK_STATUS
+    gl.getProgramParameter=function(program,pname){
+        switch(pname){
+            case gl.LINK_STATUS:
+            return true;
+        }
+        return true;
+    }
+
+    //getAttribLocation(program: WebGLProgram | null, name: string): number;
+    /*
+    aVertexPosition:0   aVertexNormal  aVertexColor aTextureCoord  aSkinWeight
+    */
+
+    //getUniformLocation(program: WebGLProgram | null, name: string): WebGLUniformLocation | null;
+    /*
+    uSampler0  uSampler1   uSampler2  uJoint0   uJoint1 uJoint2  uJoint3 uJoint0InvTranspose   uCurrentTime  uCurrentJellyfishTime
+    */
+}
+
 function initWin(w,h){
     w=canvas.width =  document.body.clientWidth|| window.innerWidth;
     h=canvas.height = document.body.clientHeight || window.innerHeight;
-
     try {
         gl = canvas.getContext( "experimental-webgl" ) ;
         gl.viewportWidth = w;
@@ -17,6 +46,7 @@ function initWin(w,h){
     if (!gl) {
         alert("Your browser failed to initialize WebGL.");
     }
+    patchGL(gl);
 }
 
 window.onresize =function() {
